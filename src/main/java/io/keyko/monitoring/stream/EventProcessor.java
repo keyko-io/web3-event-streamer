80d4662b736a34bcc3c18f21443156ecea0eba13
eruizgar91 <enrique@keyko.io>
2020-02-10 11:34:17 +0100

    Move celo logic to an independent project

--- src/main/java/io/keyko/monitoring/stream/EventProcessor.java
+++ src/main/java/io/keyko/monitoring/stream/EventProcessor.java
@@ -1,25 +1,15 @@
 package io.keyko.monitoring.stream;
 
-import io.keyko.monitoring.model.AccountCreatedAggregation;
-import io.keyko.monitoring.time.EventBlockTimestampExtractor;
-import io.keyko.monitoring.windows.DailyTimeWindows;
 import net.consensys.eventeum.BlockEvent;
 import net.consensys.eventeum.ContractEvent;
 import net.consensys.eventeum.EventBlock;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.utils.Bytes;
-import org.apache.kafka.streams.KeyValue;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.*;
-import org.apache.kafka.streams.state.WindowStore;
+import org.apache.kafka.streams.kstream.Joined;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
 
-import java.time.Duration;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.util.List;
-
-import static org.apache.kafka.streams.kstream.Suppressed.BufferConfig.unbounded;
 
 public class EventProcessor {
 
@@ -74,76 +64,4 @@ public class EventProcessor {
       .selectKey((key, event) -> event.getId());
   }
 
-
-  /**
-   * Aggregate accounts created events using a daily window
-   *
-   * @param topicsToAggregate
-   * @param builder
-   * @param eventBlockAvroSerde
-   * @return
-   */
-  public KStream<String, AccountCreatedAggregation> accountDailyAggregation(List<String> topicsToAggregate, StreamsBuilder builder, Serde<EventBlock> eventBlockAvroSerde) {
-
-    final int windowStartHour = 0;
-    final ZoneId zone = ZoneOffset.UTC;
-    final Duration gracePeriod = Duration.ofMinutes(60L);
-
-    List<String> accountsTopics = topicsToAggregate;//, "VoteSignerAuthorized".toLowerCase(), "AttestationSignerAuthorized".toLowerCase());
-
-    KStream<String, EventBlock> accountsCreatedStream = builder.stream(accountsTopics,
-      Consumed.with(Serdes.String(), eventBlockAvroSerde)
-        .withTimestampExtractor(new EventBlockTimestampExtractor()));
-
-    KTable<Windowed<String>, Long> accountsCreatedDayTable =
-      accountsCreatedStream
-        .selectKey((key, event) -> event.getDetails().getName())
-        .groupByKey(Grouped.with(Serdes.String(), eventBlockAvroSerde))
-        .windowedBy(new DailyTimeWindows(zone, windowStartHour, gracePeriod))
-        // .windowedBy(TimeWindows.of(Duration.ofSeconds(10)))
-        .count(Materialized.<String, Long, WindowStore<Bytes, byte[]>>with(Serdes.String(), Serdes.Long())
-          // the default store retention time is 1 day;
-          // need to explicitly increase the retention time
-          // to allow for a 1-day window plus configured grace period
-          .withRetention(Duration.ofDays(1L).plus(gracePeriod)))
-        // emits the final count when the window is closed.
-        .suppress(Suppressed.untilWindowCloses(unbounded()));
-    ;
-
-    return formatAccountCreatedAggregation(accountsCreatedDayTable, zone);
-
-  }
-
-
-  /**
-   * Transforms a Windowed Aggregation Table in a Stream of formatted data
-   *
-   * @param accountsCreatedDayTable
-   * @param zone
-   * @return
-   */
-  private KStream<String, AccountCreatedAggregation> formatAccountCreatedAggregation(KTable<Windowed<String>, Long> accountsCreatedDayTable, ZoneId zone) {
-
-    return accountsCreatedDayTable
-      .toStream()
-      .map((windowed, count) -> {
-
-        AccountCreatedAggregation accountCreatedAggregation = new AccountCreatedAggregation(
-          windowed.window().startTime().atZone(zone),
-          count,
-          windowed.key()
-
-        );
-
-        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm:ss Z");
-        //String formattedString = windowed.window().startTime().atZone(ZoneOffset.UTC).format(formatter);
-
-        //String key = windowed.key().concat("-").concat(formattedString);
-
-        String key = windowed.key().concat("-").concat(accountCreatedAggregation.getDate());
-        return KeyValue.pair(key, accountCreatedAggregation);
-
-      });
-
-  }
 }
